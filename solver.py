1c73b67f7d02ffb404e838f0eb0751ea4e8dd7b1
axelmusch <44018738+axelmusch@users.noreply.github.com>
2022-03-22 14:04:56 +0100

    somewhat works with word input

--- solver.py
+++ solver.py
@@ -1,6 +1,6 @@
 import random
 
-currentWord = "sight"
+currentWord = "slosh"
 
 def load_words(WORDLIST_FILENAME):
        print ("Loading word list from file...")
@@ -20,53 +20,77 @@ def choose_word (wordlist):
 
 wordlist = load_words('words.txt')
 
-guess1 = "night"
+guess1 = "sloan"
 
-count = 1
+
 guess_letters_good = list()
+guess_letters_badPlace = [list(),list(),list(),list(),list()]
 guess_letters_bad = list()
 guess_letters_place = ["-","-","-","-","-"]
 
-
-for i in guess1:
-    if i in currentWord:
-        guess_letters_good.append(i)
-        if i == currentWord[count-1]:
-            #print(i, " correct letter at place " ,count)
-            guess_letters_place[count-1] = i
-        else:
-           print(i, " correct letter wrong place ")
-    else:
-        guess_letters_bad.append(i)
-    count = count +1
-
-print(guess_letters_place)
-print("good letters",guess_letters_good)
-print("bad letters",guess_letters_bad)
-
-newlist = list()
-
-for word in wordlist:
-    matchall = list()
-    for idx,letter in enumerate(guess_letters_place,start=0):
-        #print(idx, letter)
-        
-        if letter != "-":
-            if letter == word[idx]:
-                #print("start with correct letter", word)
-                matchall.append(True)
+for guessCount in range(1, 7):
+    print('__________________________________')
+    print("start guess %d" % (guessCount))
+    count = 1
+    for i in guess1:
+        if i in currentWord:
+            if i not in guess_letters_good:
+                guess_letters_good.append(i)
+            if i == currentWord[count-1]:
+                #print(i, " correct letter at place " ,count)
+                guess_letters_place[count-1] = i
             else:
+                #print(i, " correct letter wrong place ")
+                if i not in guess_letters_badPlace[count-1]:
+                    guess_letters_badPlace[count-1].append(i)
+        else:
+            if i not in guess_letters_bad:
+                guess_letters_bad.append(i)
+        count = count +1
+
+    print(guess_letters_place)
+    print("good letters",guess_letters_good)
+    print("goodbad letters",guess_letters_badPlace)
+    print("bad letters",guess_letters_bad)
+
+    newlist = list()
+
+    for word in wordlist:
+        matchall = list()
+
+        for idx,letter in enumerate(guess_letters_good,start=0):
+            if letter not in word:
                 matchall.append(False)
+        for idx,letter in enumerate(guess_letters_place,start=0):
+            #print(idx, letter)
+            
+            if letter != "-":
+                if letter == word[idx]:
+                    #print("start with correct letter", word)
+                    matchall.append(True)
+                else:
+                    matchall.append(False)
 
-    checkAll = True
-    for val in matchall:
-        if val == False:
-            checkAll = False
+        checkAll = True
+        for val in matchall:
+            if val == False:
+                checkAll = False
 
-    if checkAll == True:
-        
-        for badletter in guess_letters_bad:
-            if badletter not in word:
-                print(word)
-                print("---")
+        if checkAll == True:
+            checkBadLetter = True
+            for badletter in guess_letters_bad:
+                if badletter in word:
+                    checkBadLetter = False
+            if checkBadLetter == True:
+                newlist.append(word)
+
+    print('possible words after guess %d :' % (guessCount), guess1)    
+    print(newlist)
+    newguess = random.choice(newlist)
+   
+    print("chosen word for guess %d: " % (guessCount+1), newguess)
+    guess1 = newguess
+    if newguess == currentWord:
+        print("word found after %d guesses: " % (guessCount+1), newguess)
+        break
     
